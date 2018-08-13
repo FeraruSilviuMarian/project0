f3149ad1413a1d4a296484578e3b4b58f67b1c3b
Feraru Silviu Marian <silvermirai@yahoo.com>
2018-08-13 12:47:42 +0000

    changed footer

--- Scripts/basic.js
+++ Scripts/basic.js
@@ -1,7 +1,7 @@
 // footer change event based on the size of window
 function footerChange(x) {
     if (x.matches) { // If media query matches
-        $("#footerIndex").html("small");
+        $("#footerIndex").html('<a href="https://play.google.com/store/apps/details?id=com.squareenix.lis&hl=en">Get it on android</a><a href="https://itunes.apple.com/us/app/life-is-strange/id1180101534?mt=8">Get it on iphone</a>');
     } else {
         $("#footerIndex").html("big");
     }
