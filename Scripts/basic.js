ab7a0549f7852e9cec896b61d8aa9878a78e3117
Feraru Silviu Marian <silvermirai@yahoo.com>
2018-08-13 12:45:31 +0000

    changed function name

--- Scripts/basic.js
+++ Scripts/basic.js
@@ -1,11 +1,11 @@
-function myFunction(x) {
+// footer change event based on the size of window
+function footerChange(x) {
     if (x.matches) { // If media query matches
         $("#footerIndex").html("small");
     } else {
         $("#footerIndex").html("big");
     }
 }
-
 var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+footerChange(x) // Call listener function at run time
+x.addListener(footerChange) // Attach listener function on state changes
